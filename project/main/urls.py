f91376d3d523b5200b285c981d42276faf313c3e
Serjik07 <serjik.h1@tumo.org>
2023-08-13 19:10:36 +0400

    debbug

--- project/main/urls.py
+++ project/main/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("",views.home),
     path("home",views.home, name="home"),
     path("shop",views.shop, name="shop"),
-    path("login",views.sign_in),
+    path("login",views.sign_in,name="login"),
     path("logout",views.logoout, name="logout"),
     path("sign_up", views.sign_up, name="sign_up"),
     path("post",views.createPost, name="post"),
